88da4d01399acfc8e1fdde91dfccda8f70e3f847
inum9 <inum9@outlook>
2025-05-16 01:37:36 +0530

    updating the controllers

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -124,4 +124,69 @@ const userlogOut= asyncHandler(async(req,res)=>{
 
 });
 
-export { userRegister,userLogin,userlogOut };
+const refreshAccessToken = asyncHandler(async (req, res) => {
+    const incomingRefreshToken = req.cookies.refreshToken || req.body.refreshToken
+
+    if (!incomingRefreshToken) {
+        throw new ApiError(401, "unauthorized request")
+    }
+
+    try {
+        const decodedToken = jwt.verify(
+            incomingRefreshToken,
+            process.env.REFRESH_TOKEN_SECRET
+        )
+    
+        const user = await User.findById(decodedToken?._id)
+    
+        if (!user) {
+            throw new ApiError(401, "Invalid refresh token")
+        }
+    
+        if (incomingRefreshToken !== user?.refreshToken) {
+            throw new ApiError(401, "Refresh token is expired or used")
+            
+        }
+    
+        const options = {
+            httpOnly: true,
+            secure: true
+        }
+    
+        const {accessToken, newRefreshToken} = await generateAccessAndRefereshTokens(user._id)
+    
+        return res
+        .status(200)
+        .cookie("accessToken", accessToken, options)
+        .cookie("refreshToken", newRefreshToken, options)
+        .json(
+            new ApiResponse(
+                200, 
+                {accessToken, refreshToken: newRefreshToken},
+                "Access token refreshed"
+            )
+        )
+    } catch (error) {
+        throw new ApiError(401, error?.message || "Invalid refresh token")
+    }
+
+});
+
+const changeCurrentPassword=asyncHandler(async(req,res)=>{
+    const {oldPassword,newPassword}=req.body;
+     const user = await User.findById(req.user?._id)
+    const isPasswordCorrect = await user.isPasswordCorrect(oldPassword)
+
+    if (!isPasswordCorrect) {
+        throw new ApiError(400, "Invalid old password")
+    }
+      user.password= newPassword;
+       await user.save({validateBeforeSave: false});
+
+       return res
+    .status(200)
+    .json(new ApiResponse(200, {}, "Password changed successfully"));
+
+});
+
+export { userRegister,userLogin,userlogOut,refreshAccessToken,changeCurrentPassword };
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { userLogin, userlogOut, userRegister } from "../controller/user.controller.js";
+import { changeCurrentPassword, refreshAccessToken, userLogin, userlogOut, userRegister } from "../controller/user.controller.js";
 import { verifyJwt } from "../middleware/auth,middleware.js";
 const rout=Router();
 
@@ -8,5 +8,7 @@ rout.route("/login").post(userLogin);
 
 //secured routes
 rout.route("/logout").post(verifyJwt,userlogOut);
+rout.route("/refresh-token").post(refreshAccessToken);
+rout.route("/change-Password").post(verifyJwt,changeCurrentPassword);
 
 export default rout;
