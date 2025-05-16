3cfe281410c967f6baecfa3620f4740dea1c1b96
inum9 <inum9@outlook>
2025-05-16 22:43:37 +0530

    port

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import dotenv  from "dotenv";
 import app from "../app.js";
 import connectDb from "./config/db.js";
-const port= process.env.Port||8000;
+const port= process.env.PORT||8000;
 
 dotenv.config({
     path: './.env'
