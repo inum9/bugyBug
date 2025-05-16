2d189ec833378418b90b4c53564ee5b4074ab8e4
inum9 <inum9@outlook>
2025-05-16 22:42:13 +0530

    fix: bind to process.env.PORT for Render deployment

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import dotenv  from "dotenv";
 import app from "../app.js";
 import connectDb from "./config/db.js";
+const port= process.env.Port||8000;
 
 dotenv.config({
     path: './.env'
@@ -8,8 +9,8 @@ dotenv.config({
 
 connectDb()
 .then(()=>{
-    app.listen(process.env.Port,()=>{
-        console.log(`server is running on port ${process.env.PORT}`);
+    app.listen(port,()=>{
+        console.log(`server is running on port ${port}`);
         
     })
 });
